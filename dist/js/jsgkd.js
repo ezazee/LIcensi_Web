4f3aa88b406674292a45b8aaae98ed9cd0d4efd4
ezazee <yukiirima123@gmail.com>
2022-10-04 16:00:51 +0700

    first commit

---
+++ dist/js/jsgkd.js
@@ -0,0 +1,9 @@
+/// JS PT GRAHA KARA DIGITAL
+
+$('#tabelku').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+});
+$('.buttons-copy, .buttons-csv, .buttons-print, .buttons-pdf, .buttons-excel').addClass('btn btn-primary mr-1');
